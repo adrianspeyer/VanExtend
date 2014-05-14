cc3256eef74f42cdd0e37d8c86da8d0f92f04877
Adrian <adriansonline@users.noreply.github.com>
2014-05-14 11:49:52 -0400

    Update class.hidecomments.plugin.php
    to do : add category event for permissions

--- Plugins/HideComments/HideComments/class.hidecomments.plugin.php
+++ Plugins/HideComments/HideComments/class.hidecomments.plugin.php
@@ -1,11 +1,11 @@
 <?php if (!defined('APPLICATION')) exit();
 
-// Define the plugin:
+// Mucho thanks to Kasper
 $PluginInfo['HideComments'] = array(
    'Name' => 'Hide Comments',
    'Description' => 'This allows forum owners to hide comments from some, but allows discussion to be seen.',
    'Version' => '1.0',
-   'RegisterPermissions' => array('Plugins.HideComments.View' => 1),
+   'RegisterPermissions' => array('Vanilla.Comment.View' => 'Vanilla.Discussion.View'),
    'Author' => "Adrian",
    'AuthorUrl' => 'http://www.vanillaforums.com',
    'MobileFriendly' => TRUE,
@@ -14,18 +14,11 @@ $PluginInfo['HideComments'] = array(
 );
 
 class HideCommentsPlugin extends Gdn_Plugin {
-
-public function DiscussionController_AfterCommentFormat_Handler($Sender) { //this is discussion & comments
-//public function DiscussionController_AfterCommentBody_Handler($Sender) { //comments only
-$canviewcomments = Gdn::Session()->CheckPermission('Plugins.HideComments.View');
-if ($canviewcomments) {
- }
- else
- { 
-//$Sender->EventArguments['Object']->FormatBody = "<a href=".Url('/entry/signin')."> This message can be viewed by members only</a>";
-$Sender->EventArguments['Comment']->FormatBody= "<a href=".Url('/entry/signin')."> This message can be viewed by members only</a>";
- }
-	 }
-   public function Setup() {
-   } 
+	public function DiscussionController_AfterCommentFormat_Handler($Sender) { //this is discussion & comments
+		$canviewcomments = Gdn::Session()->CheckPermission('Vanilla.Comment.View');
+		
+		if (!$canviewcomments) { 
+			$Sender->EventArguments['Comment']->FormatBody = Anchor(T('This comment can be viewed by members only'), Url('/entry/signin'));
+		}
+	}
 }
