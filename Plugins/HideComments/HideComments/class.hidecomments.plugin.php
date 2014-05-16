70597b8f40604c21bd371172655db80a3df51f0f
adriansonline <adriansprojects@gmail.com>
2014-05-15 23:29:01 -0400

    revert back to permission, but with cleaner code.

--- Plugins/HideComments/HideComments/class.hidecomments.plugin.php
+++ Plugins/HideComments/HideComments/class.hidecomments.plugin.php
@@ -1,11 +1,14 @@
 <?php if (!defined('APPLICATION')) exit();
 
-// Mucho thanks to Kasper
+/* Mucho thanks to Kasper & Lincoln
+*/
+
 $PluginInfo['HideComments'] = array(
    'Name' => 'Hide Comments',
    'Description' => 'This allows forum owners to hide comments from some, but allows discussion to be seen.',
-   'Version' => '1.0',
-   'RegisterPermissions' => array('Vanilla.Comment.View' => 'Vanilla.Discussion.View'),
+   'Version' => '1.1',
+   'RegisterPermissions' => array('Plugins.HideComments.View' => 1),
+   //'RegisterPermissions' => array('Vanilla.Comments.View' => 'Vanilla.Discussions.View'),
    'Author' => "Adrian",
    'AuthorUrl' => 'http://www.vanillaforums.com',
    'MobileFriendly' => TRUE,
@@ -14,11 +17,12 @@ $PluginInfo['HideComments'] = array(
 );
 
 class HideCommentsPlugin extends Gdn_Plugin {
-	public function DiscussionController_AfterCommentFormat_Handler($Sender) { //this is discussion & comments
-		$canviewcomments = Gdn::Session()->CheckPermission('Vanilla.Comment.View');
-		
-		if (!$canviewcomments) { 
+
+	public function DiscussionController_AfterCommentFormat_Handler($Sender) { 
+	  //$canviewcomments = Gdn::Session()->CheckPermission('Vanilla.Comments.View');
+	  $canviewcomments = Gdn::Session()->CheckPermission('Plugins.HideComments.View');
+	    if (!$canviewcomments) { 
 			$Sender->EventArguments['Comment']->FormatBody = Anchor(T('This comment can be viewed by members only'), Url('/entry/signin'));
 		}
-	}
+	 }   
 }
--- Plugins/HideComments/README.markdown
+++ Plugins/HideComments/README.markdown
@@ -9,7 +9,7 @@ Allows you to hide comments to non-members (or roles/permissions you set to be a
 To Do - -Implement ideas from Lincoln:
 
 <ul>
-<li>Make a Vanilla.Comment.View permission per category that defaults to Vanilla.Discussion.View; and make the replacement string translatable</li>
+<li>Make a Vanilla.Comments.View permission per category that defaults to Vanilla.Discussions.View; and make the replacement string translatable</li>
 
 <li> Add a setting that says "Users without permission to view comments may only see discussion excerpt" and implement that too</li>
 </ul>
