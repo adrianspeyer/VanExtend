526cf95abe0432fcf8ae1babd050ddc41a702041
Adrian <adriansonline@users.noreply.github.com>
2014-05-24 00:48:41 -0400

    Complete Rewrite

--- Plugins/HomeAlone/class.homealone.plugin.php
+++ Plugins/HomeAlone/class.homealone.plugin.php
@@ -2,7 +2,7 @@
 
 $PluginInfo['HomeAlone'] = array(
    'Name' => 'Home Alone',
-   'Description' => 'This plugin prevents admin or system user content from being deleted.',
+   'Description' => 'This plugin creates a new permission "HomeAlone" which when used with permissions moderation.manage, esnures all content of the Admins cannot be deleted from profile page.',
    'Version' => '1.0',
    'RegisterPermissions' => array('Plugins.HomeAlone.Manage' => 1),
    'Author' => "Adrian",
@@ -14,17 +14,15 @@ $PluginInfo['HomeAlone'] = array(
 
 class HomeAlonePlugin extends Gdn_Plugin {
 
-public function UserController_BeforeDeleteContent_Handler($Sender){
-$isHomeAlone = Gdn::Session()->CheckPermission('Plugins.HomeAlone.Manage');
-if ($isHomeAlone) {
- $this->Permission('Garden.Moderation.Manage');
-     $UserModel = Gdn::UserModel();
-     $User = $UserModel->GetID($UserID);
-      if (GetValue('Admin', $User) == 2)
-         throw ForbiddenException("@You may not delete content of a System user.");
-      elseif (GetValue('Admin', $User))
-         throw ForbiddenException("@You may not delete content of an Admin.");	 
-         }  
-      }
-   
+public function ProfileController_BeforeProfileOptions_Handler($Sender) {
+	$isHomeAlone = Gdn::Session()->CheckPermission('Plugins.HomeAlone.Manage');
+		if ($isHomeAlone) {
+			$Session = Gdn::Session();
+			$Controller = Gdn::Controller();
+			$UserID = $Controller->User->UserID;
+		if($Controller->User->Admin) {
+		unset($Controller->EventArguments['ProfileOptions']);  
+				}
+			}
+		 }
 }
