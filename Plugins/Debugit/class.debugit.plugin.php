d609b05abd52df3f6d0764ef45ba836a59186991
adriansonline <adriansprojects@gmail.com>
2014-05-27 23:10:45 -0400

    Changes

--- Plugins/Debugit/class.debugit.plugin.php
+++ Plugins/Debugit/class.debugit.plugin.php
@@ -7,7 +7,7 @@ Thanks to Lincoln for the assist
 // Define the plugin:
 $PluginInfo['Debugit'] = array(
    'Name' => 'Debug It',
-   'Description' => 'This plugin allows you to turn on native Vanilla Debug mode without modifying config. Will not work if plugin/dashboard issue. In this case add <b>$Configuration[\'Debug\'] = TRUE;</b> to conf/config. Remeber to disable when done so user do not see debug issues. ',
+   'Description' => 'This plugin allows you to turn on native Vanilla Debug mode without modifying config via FTP. Will not work if plugin/dashboard issue. In this case add <b>$Configuration[\'Debug\'] = TRUE;</b> to conf/config. Remember to disable when done so user do not see debug issues. ',
    'Version' => '1.0',
    'Author' => "Adrian",
    'AuthorUrl' => 'http://www.vanillaforums.com',
