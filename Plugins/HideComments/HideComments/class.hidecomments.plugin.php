72e0da435f1dcf7e5644c93241187c1db0447b6f
adriansonline <adriansprojects@gmail.com>
2014-05-29 22:15:23 -0400

    code clean up

--- Plugins/HideComments/HideComments/class.hidecomments.plugin.php
+++ Plugins/HideComments/HideComments/class.hidecomments.plugin.php
@@ -14,49 +14,46 @@ $PluginInfo['HideComments'] = array(
 
 class HideCommentsPlugin extends Gdn_Plugin {
 
-		 public function DiscussionController_BeforeDiscussionRender_Handler($Sender, $Args) {
-				$hidecomments = Gdn::Session()->CheckPermission('Plugins.HideComments.View');
-					if (!CheckPermission('Garden.Moderation.Manage')){
-						if (!Gdn::Session()->IsValid() || $hidecomments) { 
+  public function DiscussionController_BeforeDiscussionRender_Handler($Sender, $Args) {
+	$hidecomments = Gdn::Session()->CheckPermission('Plugins.HideComments.View');
+		if (!CheckPermission('Garden.Moderation.Manage')){
+			if (!Gdn::Session()->IsValid() || $hidecomments) { 
 						
-						// kills all comments -- Thanks to Linc for the assist
-						$BlankDataSet = new Gdn_DataSet();
-						$Sender->SetData('Comments', $BlankDataSet);
-							
+				// kills all comments -- Thanks to Linc for the assist
+				$BlankDataSet = new Gdn_DataSet();
+				$Sender->SetData('Comments', $BlankDataSet);
+					
 
-						//closes discussions
-						 $Discussion = $Sender->Data('Discussion');
-						 SetValue('Closed', $Discussion, 1);  
-						 $Sender->SetData('Discussion', $Discussion);
+				//closes discussions
+				 $Discussion = $Sender->Data('Discussion');
+				 SetValue('Closed', $Discussion, 1);  
+				 $Sender->SetData('Discussion', $Discussion);
 			}
 		}
 	}
 
-       public function DiscussionController_AfterDiscussion_Handler($Sender) { 
-		
-			$hidecomments = Gdn::Session()->CheckPermission('Plugins.HideComments.View');
-				if (!CheckPermission('Garden.Moderation.Manage')){
-					if (!Gdn::Session()->IsValid() || $hidecomments) { 
-						echo "<div class='Foot Closed'>".(T('Comment on this discussion can be viewed by members only. ')).Anchor(T('Apply for membership.'), Url('../entry/signin'));".</div>";
+   public function DiscussionController_AfterDiscussion_Handler($Sender) { 
+	 $hidecomments = Gdn::Session()->CheckPermission('Plugins.HideComments.View');
+		if (!CheckPermission('Garden.Moderation.Manage')){
+			if (!Gdn::Session()->IsValid() || $hidecomments) { 
+				echo "<div class='Foot Closed'>".(T('Comment on this discussion can be viewed by members only. ')).Anchor(T('Apply for membership.'), Url('../entry/signin'));".</div>";
 					}
 				}	
 			}
-	 
-		public function DiscussionController_AfterCommentFormat_Handler($Sender) { 
-		
-			$hidecomments = Gdn::Session()->CheckPermission('Plugins.HideComments.View');
-				if (!CheckPermission('Garden.Moderation.Manage')){
-					if (!Gdn::Session()->IsValid() || $hidecomments) { 
+
+	public function DiscussionController_AfterCommentFormat_Handler($Sender) { 		
+		$hidecomments = Gdn::Session()->CheckPermission('Plugins.HideComments.View');
+			if (!CheckPermission('Garden.Moderation.Manage')){
+			   if (!Gdn::Session()->IsValid() || $hidecomments) { 
 	
-					//clean up css
-					echo '<style>
-					div.CommentsWrap{display:none;}
-					div.Note.Closed{display:none;}
-					div.note.Closed.SignInOrRegister{display:none;}
-					div.MessageForm {display:none;}
-					</style>';
-						}
+				//clean up css
+				echo '<style>
+				div.CommentsWrap{display:none;}
+				div.Note.Closed{display:none;}
+				div.note.Closed.SignInOrRegister{display:none;}
+				div.MessageForm {display:none;}
+				</style>';
 					}
+				}
 			}
-     
 }
