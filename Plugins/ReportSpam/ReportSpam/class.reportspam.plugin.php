2b8ea872884d8c57bfe9e56fc201a763f4ed5cf6
adriansonline <adriansprojects@gmail.com>
2014-11-22 00:11:18 -0500

    refactor

--- Plugins/ReportSpam/ReportSpam/class.reportspam.plugin.php
+++ Plugins/ReportSpam/ReportSpam/class.reportspam.plugin.php
@@ -1,8 +1,8 @@
 <?php
 if (!defined('APPLICATION')) { exit(); }
 
+//Thanks to John for patience and time
 
-// Define the plugin:
 $PluginInfo['ReportSpam'] = array(
     'Name' => 'Report Spam',
     'Description' => 'Reports Spam to Stop Forum Spam.',
@@ -15,6 +15,13 @@ $PluginInfo['ReportSpam'] = array(
 
 class ReportSpamPlugin extends Gdn_Plugin {
     public function Base_DiscussionOptions_Handler($Sender, $Args) {
+		
+		$UserModel = Gdn::UserModel();
+		$User = $UserModel->GetID($Args['Discussion']->InsertUserID);
+		if ($UserModel->CheckPermission($User, 'Garden.Moderation.Manage')) { 
+			return;
+		}
+		
         if (Gdn::Session()->CheckPermission('Garden.Moderation.Manage')) {
             $Sender->EventArguments['DiscussionOptions']['ReportSpam'] = array(
                 'Label' => T('Report Forum Spam'),
@@ -25,7 +32,14 @@ class ReportSpamPlugin extends Gdn_Plugin {
     }
 
     public function DiscussionController_CommentOptions_Handler($Sender, $Args) {
-        if (Gdn::Session()->CheckPermission('Garden.Moderation.Manage')) {
+        
+		$UserModel = Gdn::UserModel();
+		$User = $UserModel->GetID($Args['Comment']->InsertUserID);
+		if ($UserModel->CheckPermission($User, 'Garden.Moderation.Manage')) { 
+			return;
+		}
+		
+		if (Gdn::Session()->CheckPermission('Garden.Moderation.Manage')) {
             $Sender->EventArguments['CommentOptions']['ReportSpam'] = array(
                 'Label' => T('Report Forum Spam'),
                 'Url' => '/discussion/SFSOptions/comment/' . $Args['Comment']->CommentID . '/#Comment_' . $Args['Comment']->CommentID,
@@ -55,19 +69,9 @@ class ReportSpamPlugin extends Gdn_Plugin {
 		$content = getRecord($context, $contextID);
 		$Sender->setData('content', $content);
 		$Sender->Form = new Gdn_Form();
-
-		$UserModel = Gdn::UserModel();
-			$User = $UserModel->GetID($content['InsertUserID']);
-			if ($UserModel->CheckPermission($User, 'Garden.Moderation.Manage')) { 
-			$Sender->Form->AddError(T('You cannot report a moderator.'));	
-			}
 				
 		if ($Sender->Form->AuthenticatedPostBack() === true) {
 			if ($Sender->Form->ErrorCount() == 0) {
-				$FormValues = $Sender->Form->FormValues();
-				$contextID = val('contextID', $FormValues);
-				$context = val('context', $FormValues);
-				$content = getRecord($context, $contextID);
 				$this->SendToSFS($content['InsertName'], $content['InsertIPAddress'], $content['InsertEmail'], $content['Body']);
 			    $Sender->InformMessage(T('Your spam report has been sent.'), 'Dismissable');
 			}
@@ -86,68 +90,6 @@ class ReportSpamPlugin extends Gdn_Plugin {
 		$Sender->Form->SetData($Data);
 
 		$Sender->Render('sfsoptions', '', 'plugins/ReportSpam');
-    }
-
-    public function CommentController_SFSOptions_Create($Sender, $Args) {
-
-        if (Gdn::Session()->CheckPermission('Garden.Moderation.Manage')) {
-
-            //APIkey
-            $SFSkey = C('Plugins.ReportSpam.APIKey');
-
-            //get arguments
-            if (count($Sender->RequestArgs) != 2) {
-                throw new Gdn_UserException('Bad Request', 400);
-            }
-
-            list($context, $contextID) = $Sender->RequestArgs;
-            $content = getRecord($context, $contextID);
-            $Sender->setData('content', $content);
-            $Sender->Form = new Gdn_Form();
-
-            if ($Sender->Form->AuthenticatedPostBack() === true) {
-                if ($Sender->Form->ErrorCount() == 0) {
-                    $FormValues = $Sender->Form->FormValues();
-                    $contextID = val('contextID', $FormValues);
-                    $context = val('context', $FormValues);
-                    $content = getRecord($context, $contextID);
-                    //var_dump('SampleData:', $content);
-                    $this->PostToHost(
-                        "username=" . urlencode($content['InsertName']) . "&ip_addr=" . urlencode(
-                            $content['InsertIPAddress']
-                        ) . "&email=" . urlencode(
-                            $content['InsertEmail']
-                        ) . "&api_key=" . $SFSkey . "&evidence=" . urlencode($content['Body'])
-                    );
-                }
-            } else {
-                $Sender->Form->AddHidden('context', $context);
-                $Sender->Form->AddHidden('contextID', $contextID);
-            }
-
-            $Title = t('Stop Forum Spam Report');
-            $Data = array(
-                'Title' => $Title
-            );
-
-
-            $Sender->SetData($Data);
-            $Sender->Form->SetData($Data);
-
-            $Sender->Render('sfsoptions', '', 'plugins/ReportSpam');
-
-        }
-    }
-
-    public function PostToHost($data) {
-        $fp = fsockopen("www.stopforumspam.com", 80);
-        fputs($fp, "POST /add.php HTTP/1.1\n");
-        fputs($fp, "Host: www.stopforumspam.com\n");
-        fputs($fp, "Content-type: application/x-www-form-urlencoded\n");
-        fputs($fp, "Content-length: " . strlen($data) . "\n");
-        fputs($fp, "Connection: close\n\n");
-        fputs($fp, $data);
-        fclose($fp);
     }
 
     public function SendToSFS($Username, $IP, $Email, $Evidence) {
