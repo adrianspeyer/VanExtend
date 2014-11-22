1dcc1df0d045f361c4fa55094938a23428a821bf
adriansonline <adriansprojects@gmail.com>
2014-11-22 14:49:59 -0500

    Add option to delete on pop-up
    TODO: make it actually delete.

--- Plugins/ReportSpam/ReportSpam/views/sfsoptions.php
+++ Plugins/ReportSpam/ReportSpam/views/sfsoptions.php
@@ -4,5 +4,11 @@ echo $this->Form->Open();
 echo $this->Form->Errors();
 ?>
 Report this user to Stop Forum Spam
+<?php
+   echo '<div class="P">', $this->Form->CheckBox('DeleteContent', T("Also delete this content.")), '</div>';
+//https://github.com/vanilla/vanilla/blob/0321c0551fb2ead494bbef0b4126a29b9c4880f5/applications/vanilla/views/discussion/helper_functions.php#L238
+//https://github.com/vanilla/vanilla/blob/0321c0551fb2ead494bbef0b4126a29b9c4880f5/applications/vanilla/views/discussion/helper_functions.php#L328
+?>
 <?php echo $this->Form->Close('Submit', '', array('class' => 'Button BigButton'));
 ?>
+	
