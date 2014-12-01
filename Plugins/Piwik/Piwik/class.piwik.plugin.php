639ea8ebe4a30323c1f613cddaedb9fc55ac7475
Adrian <adrianspeyer@users.noreply.github.com>
2014-12-01 13:43:00 -0500

    Update class.piwik.plugin.php
    Changes to tracking

--- Plugins/Piwik/Piwik/class.piwik.plugin.php
+++ Plugins/Piwik/Piwik/class.piwik.plugin.php
@@ -4,7 +4,7 @@ $PluginInfo['Piwik'] = array(
 	'Name' => 'Piwik Analytics',
 	'Description' => 'Piwik Analytics. Adds Piwik to footer. Requires themes with AfterBody event. Also allows you to see your stats from your Vanilla Install.',
 	'MobileFriendly' => TRUE,
-	'Version' => '2.6.5',
+	'Version' => '2.6.6',
 	'Author' => 'Adrian Speyer',
 	'AuthorEmail' => 'adriansprojects+vanilla@gmail.com',
 	'AuthorUrl' => 'http://www.adrianspeyer.com/',
@@ -30,8 +30,8 @@ class PiwikPlugin implements Gdn_IPlugin {
 		$ConfigurationModule = new ConfigurationModule($Sender);
 		$ConfigurationModule->RenderAll = True;
 		$Schema = array(
-		'Plugins.Piwik.PiwikInstall' => array('LabelCode' => 'PiwikInstall', 'Control' => 'TextBox', 'Default' => C('Plugins.Piwik.PiwikInstall', 'http://www.example.com/piwik/')),
-        'Plugins.Piwik.PiwikSiteID' => array('LabelCode' => 'PiwikSiteID', 'Control' => 'TextBox', 'Default' => C('Plugins.Piwik.PiwikSiteID', '1')),
+		'Plugins.Piwik.PiwikInstall' => array('LabelCode' => 'PiwikInstall', 'Control' => 'TextBox', 'Default' => C('Plugins.Piwik.PiwikInstall', '//www.example.com/piwik/')),
+                'Plugins.Piwik.PiwikSiteID' => array('LabelCode' => 'PiwikSiteID', 'Control' => 'TextBox', 'Default' => C('Plugins.Piwik.PiwikSiteID', '1')),
 		'Plugins.Piwik.PiwikAuthKey' => array('LabelCode' => 'PiwikAuthKey', 'Control' => 'TextBox', 'Default' => C('Plugins.Piwik.PiwikAuthKey', ''))
 	);
 		$ConfigurationModule->Schema($Schema);
@@ -40,8 +40,6 @@ class PiwikPlugin implements Gdn_IPlugin {
 		$Sender->ConfigurationModule = $ConfigurationModule;
 		$Sender->Render();
 	}
-	
-	
 	
 	public function Base_AfterBody_Handler($Sender) {
 		if ($Sender->MasterView == 'admin') return;
@@ -62,26 +60,24 @@ class PiwikPlugin implements Gdn_IPlugin {
 		if ($PiwikInstall) 
 			echo 
 		<<<Piwik
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(["trackPageView"]);
-  _paq.push(["enableLinkTracking"]);
-
-  (function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "$PiwikInstall";
-    _paq.push(["setTrackerUrl", u+"piwik.php"]);
-    _paq.push(["setSiteId", "$PiwikSiteID"]);
-    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<noscript>
-<!-- Piwik Image Tracker -->
-<img src="$ImageTrack/piwik.php?idsite=$PiwikSiteID&amp;rec=1" style="border:0" alt="" />
-<!-- End Piwik -->
-</noscript>
-<!-- End Piwik Code -->
+		    <!-- Piwik -->
+		    <script type="text/javascript">
+		    var _paq = _paq || [];
+		    _paq.push(['trackPageView']);
+		    _paq.push(['enableLinkTracking']);
+		    (function() {
+		    var u="$PiwikInstall";
+		    _paq.push(['setTrackerUrl', u+'piwik.php']);
+		    _paq.push(['setSiteId', '$PiwikSiteID']);
+		    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+		    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+		    })();
+		    </script>
+		    <noscript>
+		    <!-- Piwik Image Tracker -->
+		    <img src="$ImageTrack/piwik.php?idsite=$PiwikSiteID" style="border:0" alt="" />
+		    </noscript>
+		    <!-- End Piwik Code -->
 Piwik;
 	}
 	public function Setup() {
