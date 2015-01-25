2ff8f865f361aad391371664cb8e96a38dcc5eb7
Adrian <adrianspeyer@users.noreply.github.com>
2015-01-24 23:19:28 -0500

    Update class.amazinganalytics.plugin.php

--- Plugins/AmazingAnalytics/class.amazinganalytics.plugin.php
+++ Plugins/AmazingAnalytics/class.amazinganalytics.plugin.php
@@ -1,6 +1,4 @@
 <?php if (!defined('APPLICATION')) exit();
-//Custom Variable info for GA at : https://developers.google.com/analytics/devguides/collection/gajs/gaTrackingCustomVariables
-// Define the plugin:
 $PluginInfo['AmazingAnalytics'] = array(
    'Name' => 'Amazing Analytics',
    'Description' => 'Amazing Analytics provide extra data into your Google Analytics, and works only with UNIVERSAL ANALYTICS. It will add data on which Category are most viewed(as events). You will need to create two dimensions in your Google Analytics console. The first dimension will determine if a user is logged in vs not logged in. Scope type should be "session".The second dimension, will advise you the highest RoleType of visitor. Scope type used should be "User". By default we take slots 1 and 2. If these slots are already takenyou will need to modify the code according to the proper dimensions to use. Learn more about Google Analytics dimensions in Universal Aanalytics here: https://developers.google.com/analytics/devguides/platform/customdimsmets.',
