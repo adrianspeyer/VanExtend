13d1048cbffb915cfd506b8374f5ff5c0a24cbb1
adrianspeyer <adriansprojects@gmail.com>
2015-02-24 21:38:49 -0500

    code cleanup

--- Plugins/20share/class.20share.plugin.php
+++ Plugins/20share/class.20share.plugin.php
@@ -15,22 +15,22 @@ $PluginInfo['20Share'] = array(
 		  $Sender->Head->AddCss('/plugins/20share/design/20share.css');
 		 }
 	   
-	   public function DiscussionsController_AfterDiscussionTitle_Handler($Sender) {
+	public function DiscussionsController_AfterDiscussionTitle_Handler($Sender) {
 		     $DiscussionUrl = $Sender->EventArguments['DiscussionUrl'];   
 			 $DiscussionTitle = $Sender->EventArguments['DiscussionName'];
 			 echo'
-		<ul class="NinetySocialBookmarks">
-		<li class="AddTo">
-		 <a target="_blank" href="http://reddit.com/submit?url='.urlencode($DiscussionUrl).'&amp;title='.urlencode($DiscussionTitle).'"><img src="./plugins/20share/design/reddit.png" width="16" height="16" alt="Add to Reddit" title="Add to Reddit" /></a>
-		 </li>
-		 <li class="AddTo">
-		 <a target="_blank" href="https://delicious.com/save?v=5&amp;provider=Vanilla&amp;noui=1&amp;jump=close&amp;url='.urlencode($DiscussionUrl).'&amp;title='.urlencode($DiscussionTitle).'"><img src="./plugins/20share/design/delicious.png" width="16" height="16" alt="Add to delicious" title="Add to delicious" /></a>
-		 </li>
-		  <li class="AddTo">
-         <a target="_blank" href="http://digg.com/submit?phase=2&amp;url='.urlencode($DiscussionUrl).'"><img src="./plugins/20share/design/digg.png" width="16" height="16" alt="Digg it" title="Add to Digg" /></a>
-		 </li>
-		 </ul>';
-			}
+				<ul class="NinetySocialBookmarks">
+					<li class="AddTo">
+						<a target="_blank" href="http://reddit.com/submit?url='.urlencode($DiscussionUrl).'&amp;title='.urlencode($DiscussionTitle).'"><img src="./plugins/20share/design/reddit.png" width="16" height="16" alt="Add to Reddit" title="Add to Reddit" /></a>
+					 </li>
+					 <li class="AddTo">
+						<a target="_blank" href="https://delicious.com/save?v=5&amp;provider=Vanilla&amp;noui=1&amp;jump=close&amp;url='.urlencode($DiscussionUrl).'&amp;title='.urlencode($DiscussionTitle).'"><img src="./plugins/20share/design/delicious.png" width="16" height="16" alt="Add to delicious" title="Add to delicious" /></a>
+					 </li>
+					 <li class="AddTo">
+						<a target="_blank" href="http://digg.com/submit?phase=2&amp;url='.urlencode($DiscussionUrl).'"><img src="./plugins/20share/design/digg.png" width="16" height="16" alt="Digg it" title="Add to Digg" /></a>
+					 </li>
+				 </ul>';
+	}
 }
 	
 	
