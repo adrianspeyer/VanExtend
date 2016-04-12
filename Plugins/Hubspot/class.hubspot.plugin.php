02b1b3c72999eb5ee12dd725d76b12ea03e4ea2d
lincolnwebs <lincolnwebs@gmail.com>
2016-04-12 17:20:29 -0400

    Hubspot plugin refactor

--- Plugins/Hubspot/class.hubspot.plugin.php
+++ Plugins/Hubspot/class.hubspot.plugin.php
@@ -3,8 +3,8 @@
 $PluginInfo['Hubspot'] = array(
    'Name' => 'Hubspot',
    'Description' => 'Adds Hubspot tracking, and new users to your Hubspot contacts list. We use Hubspot Events JavaScript API to achieve this.
-Events are only available to Hubspot Enterprise customers.',
-   'Version' => '1.0.3',
+        Events are only available to Hubspot Enterprise customers.',
+   'Version' => '1.0.4',
    'SettingsUrl' => '/settings/hubspot',
    'SettingsPermission' => 'Garden.Settings.Manage',
    'RequiredApplications' => array('Vanilla' => '2.1'),
@@ -13,65 +13,64 @@ Events are only available to Hubspot Enterprise customers.',
    'AuthorUrl' => 'http://adrianspeyer.com',
 );
 
-class HubspotPlugin extends Gdn_Plugin
-{
-    public function settingsController_Hubspot_Create($Sender, $Args = array())
-    {
-        $Sender->Permission('Garden.Settings.Manage');
-        $Sender->SetData('Title', T('Hubspot Settings'));
+/**
+ * Class HubspotPlugin
+ */
+class HubspotPlugin extends Gdn_Plugin {
 
-        $Text = '<div class="Info">'
-      .T('Enter your Hubspot ID, which is your Hubspot Account number.')
-      .'</div>';
+    /**
+     * Settings page.
+     *
+     * @param $sender
+     */
+    public function settingsController_hubspot_create($sender) {
+        $sender->permission('Garden.Settings.Manage');
+        $sender->setData('Title', t('Hubspot Settings'));
 
-        $Sender->AddAsset('Content', $Text, 'MoreLink');
+        $text = '<div class="Info">'.t('Enter your Hubspot account number.').'</div>';
+        $sender->addAsset('Content', $text, 'MoreLink');
 
-        $Cf = new ConfigurationModule($Sender);
-        $Cf->Initialize(array(
-          'Plugins.Hubspot.PortalID' => array(),
-          ));
+        $cf = new ConfigurationModule($sender);
+        $cf->initialize([
+            'Plugins.Hubspot.PortalID' => [],
+        ]);
 
-        $Sender->AddSideMenu('dashboard/settings/plugins');
-        $Cf->RenderAll();
+        $sender->addSideMenu('dashboard/settings/plugins');
+        $cf->renderAll();
     }
 
-   //Render tracking
-    public function base_AfterBody_Handler($Sender)
-    {
-      $Session = Gdn::session();
-
-      if ($Session->isValid()) {
-            $usermail = Gdn::Session()->User->Email;
-            $username = Gdn::Session()->User->Name;
-         }
-
-        $PortalID = C('Plugins.Hubspot.PortalID');
-
-   //Render EMAIL TRACKING
-   if (isset($usermail, $PortalID)) {
-       echo'
-       <script>
-          var _hsq = _hsq || [];
-          _hsq.push(["identify", {
-           email: "'.$usermail.'",
-           firstname: "'.$username.'"
-         }]);
-      </script>';
-   }
-
-   if (isset($PortalID)) {
-      echo"
-        <!-- Start of Async HubSpot Analytics Code -->
-           <script type='text/javascript'>
-             (function(d,s,i,r) {
-               if (d.getElementById(i)){return;}
-               var n=d.createElement(s),e=d.getElementsByTagName(s)[0];
-               n.id=i;n.src='//js.hs-analytics.net/analytics/'+(Math.ceil(new Date()/r)*r)+'/".$PortalID.".js';
-               e.parentNode.insertBefore(n, e);
-             })(document,'script','hs-analytics',300000);
-           </script>
-      <!-- End of Async HubSpot Analytics Code -->
-      ";
+    /**
+     * Render tracking.
+     *
+     * @param $sender
+     */
+    public function base_afterBody_handler($sender) {
+        $PortalID = c('Plugins.Hubspot.PortalID');
+        if (!$PortalID) {
+            return;
         }
+
+        if (Gdn::session()->isValid()) {
+            // EMAIL TRACKING
+            $user = Gdn::session()->User;
+            echo '
+            <script>
+                var _hsq = _hsq || [];
+                _hsq.push(["identify",{email: "'.htmlspecialchars($user->Email).'", firstname: "'.htmlspecialchars($user->Name).'"}]);
+            </script>';
+        }
+
+        echo "
+        <!-- Start of Async HubSpot Analytics Code -->
+            <script type='text/javascript'>
+                (function(d,s,i,r) {
+                if (d.getElementById(i)){return;}
+                var n=d.createElement(s),e=d.getElementsByTagName(s)[0];
+                n.id=i;n.src='//js.hs-analytics.net/analytics/'+(Math.ceil(new Date()/r)*r)+'/".$PortalID.".js';
+                e.parentNode.insertBefore(n, e);
+                })(document,'script','hs-analytics',300000);
+            </script>
+        <!-- End of Async HubSpot Analytics Code -->
+        ";
     }
 }
