4b891d18bdf2593c4ba9a02b4da188ebdee15da7
Adrian <adrianspeyer@users.noreply.github.com>
2016-11-02 18:27:50 -0400

    changes for Vanilla 2.3+

--- Plugins/ReportSpam/class.reportspam.plugin.php
+++ Plugins/ReportSpam/class.reportspam.plugin.php
@@ -4,8 +4,8 @@
 $PluginInfo['ReportSpam'] = array(
     'Name' => 'Report Spam',
     'Description' => 'Reports Spam to Stop Forum Spam.',
-    'Version' => '1.0.3',
-    'RequiredApplications' => array('Vanilla' => '2.1'),
+    'Version' => '1.0.4',
+    'RequiredApplications' => array('Vanilla' => '2.3'),
     'SettingsUrl' => '/settings/reportspam',
     'SettingsPermission' => 'Garden.Settings.Manage',
     'MobileFriendly' => TRUE,
@@ -131,7 +131,7 @@ class ReportSpamPlugin extends Gdn_Plugin {
 
     public function SendToSFS($Username, $IP, $Email, $Evidence, $Sender) {
 
-        $data = "username=" . urlencode($Username) . "&ip_addr=" . urlencode($IP) . "&email="
+          $data = "username=" . urlencode($Username) . "&ip_addr=".$IP."&email="
             . urlencode($Email) . "&api_key=" . C('Plugins.ReportSpam.APIKey') . "&evidence="
             . urlencode($Evidence);
 
