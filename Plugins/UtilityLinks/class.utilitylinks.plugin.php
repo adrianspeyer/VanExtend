dab8b19910b2a73a6801ddde3de40f8791409169
Adrian <adrianspeyer@users.noreply.github.com>
2017-02-19 20:28:21 -0500

    Update code for clarity

--- Plugins/UtilityLinks/class.utilitylinks.plugin.php
+++ Plugins/UtilityLinks/class.utilitylinks.plugin.php
@@ -13,23 +13,22 @@ class UpdateLinksPlugin extends Gdn_Plugin {
 
 	public function Base_GetAppSettingsMenuItems_Handler($Sender)
 	{
+
+
 	    if (Gdn::Session()->CheckPermission('Garden.Settings.Manage')) {
 			$Menu = $Sender->EventArguments['SideMenu'];
+			$Attrs = array('class' => 'Utility Links');
+			$Menu->AddItem('Utility Links', 'Utility Links', FALSE, $Attrs);
 
-			$dbuptxt = 'DB Structure Upgrade';
-			$Menu->AddLink('Utility Links', $dbuptxt, 'dashboard/utility/structure/all');
 
-			$dbupctxt = 'DB Update Counts';
-			$Menu->AddLink('Utility Links', $dbupctxt, 'dashboard/dba/counts');
-
-			$dbupbtxt = 'Utility Update';
-			$Menu->AddLink('Utility Links', $dbupbtxt, 'dashboard/utility/update');
+			$Menu->AddLink('Utility Links', T('DB Structure Upgrade'), 'dashboard/utility/structure/all');
+			$Menu->AddLink('Utility Links',T('DB Update Counts'), 'dashboard/dba/counts');
+			$Menu->AddLink('Utility Links', T('Utility Update'),'dashboard/utility/update');
 
 
 			//If Feed Discussions enabled offer check feeds
 		    if (Gdn::PluginManager()->CheckPlugin('FeedDiscussions') && C('Plugins.FeedDiscussions', TRUE)) {
-			$feedupdtxt = 'Check Feeds';
-			$Menu->AddLink('Utility Links', $feedupdtxt, '/plugin/feeddiscussions/checkfeeds');
+			$Menu->AddLink('Utility Links', T('Check Feeds'), '/plugin/feeddiscussions/checkfeeds');
 			}
 
 			//If Using Profile Extender version 3.0.2 enable profile export
@@ -37,15 +36,13 @@ class UpdateLinksPlugin extends Gdn_Plugin {
                         $version = val('Version', $chkPEInfo);
 			if ($version = '3.0.2') {
 			   if (Gdn::PluginManager()->CheckPlugin('ProfileExtender') && C('Plugins.ProfileExtender', TRUE)) {
-			   $profeptxt = 'Export User Profiles';
-			   $Menu->AddLink('Utility Links', $profeptxt, 'utility/exportProfiles');
+			   $Menu->AddLink('Utility Links',  T('Export User Profiles'), 'utility/exportProfiles');
 			  }
 			}
 
 			//If greater than 2.2 don't show
 			if (APPLICATION_VERSION < 2.2) {
-			$dbupdtxt = 'Set Default Roles';
-			$Menu->AddLink('Utility Links', $dbupdtxt, '/dashboard/role/defaultroles');
+			$Menu->AddLink('Utility Links', T('Set Default Roles'), '/dashboard/role/defaultroles');
 			     }
 			 }
 	}
