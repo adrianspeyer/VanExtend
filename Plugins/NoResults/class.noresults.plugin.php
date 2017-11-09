4c1b22cad80de053e38eff21eaac07cabe4c7553
Adrian <adrianspeyer@users.noreply.github.com>
2017-11-08 21:48:00 -0500

    Fix order for search

--- Plugins/NoResults/class.noresults.plugin.php
+++ Plugins/NoResults/class.noresults.plugin.php
@@ -2,7 +2,7 @@
 $PluginInfo['NoResults'] = array(
    'Name' => 'No Results',
    'Description' => "Allows users to continue a search Google or DuckDuckGo for content from your site.",
-   'Version' => '1.0',
+   'Version' => '1.1',
    'RequiredApplications' => array('Vanilla' => '2.1'),
    'MobileFriendly' => TRUE,
    'Author' => "Adrian Speyer",
@@ -14,7 +14,7 @@ $PluginInfo['NoResults'] = array(
 		 public function SearchController_PagerInit_Handler($Sender, $Args) {
            $host=  Gdn::Request()->Host();
 		   $SearchTerm = $_GET['Search'];
-		   echo '<b>Another Option</b>: Try your search on <a href="https://www.google.com/search?q='.$SearchTerm.'+site:'.$host.'" target="_blank">Google</a> or <a href="https://duckduckgo.com/?q='.$SearchTerm.'+site:'.$host.'" target="_blank">DuckDuckGo</a>';
+		   echo '<b>Another Option</b>: Try your search on <a href="https://www.google.com/search?q=site:'.$host.'+'.$SearchTerm.'" target="_blank">Google</a> or <a href="https://duckduckgo.com/?q=site:'.$host.'+'.$SearchTerm.'" target="_blank">DuckDuckGo</a>';
 		 }		 
 		 
 }
